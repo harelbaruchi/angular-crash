78c3177d5935cdd622f8838b9932014a263dff51
harelbaruchi <harelbaruchi@gmail.com>
2021-08-10 10:10:36 +0300

    add existing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,29 @@
+import {Task} from "./Task";
+
+export const TASKS: Task[] =[
+  {
+    id:1,
+    text:'Doctors Appointment',
+    day: 'may 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id:2,
+    text:'meeting at school',
+    day: 'may 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id:3,
+    text:'food shopping',
+    day: 'may 7th at 12:30pm',
+    reminder: false,
+  },
+  {
+    id:4,
+    text:'Liors Appointment',
+    day: 'may 8th at 2:30pm',
+    reminder: true,
+  }
+
+]
